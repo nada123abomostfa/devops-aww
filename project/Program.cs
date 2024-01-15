39c875063bea63a7ba54f471b622a9ba6e1a5fd0
nada123abomostfa <101100406+nada123abomostfa@users.noreply.github.com>
2024-01-15 13:04:34 +0200

    first commit

---
+++ project/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack1 s = new Stack1();
+            Stack1 temp = new Stack1();
+            Stack1 s2 = new Stack1();
+            s.push(5);
+            s.push(3);
+            s.push(9);
+            Console.WriteLine(s.peek());
+            Console.WriteLine(s.pop());
+            Console.WriteLine(s.peek());
+            s.push(8);
+            Console.WriteLine(s);
+            while (!s.IsEmpty())
+            {
+                Console.Write(s.peek() + " ");
+                temp.push(s.pop());
+            }
+            Console.WriteLine();
+            while (!temp.IsEmpty())
+            {
+                s.push(temp.pop());
+            }
+            Console.WriteLine(s);
+            s2.push(5);
+            s2.push(3);
+            s2.push(8);
+            Console.WriteLine(s2.Equals(s2));
+        }
+    }
+    
+}
---
+++ project/Stack1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Stack1
+    {
+        private int[] data;
+        private int top;
+        public Stack1()
+        {
+            data = new int[10];
+            top = -1;
+
+        }
+        public int pop()
+        {
+            return data[top--];
+        }
+        public int peek()
+        {
+            return data[top];
+        }
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+        public void push(int num)
+        {
+            if (top == data.Length - 1)
+            {
+                int[] temp = new int[data.Length + 5];
+                for (int i = 0; i <= top; i++)
+                {
+                    temp[i] = data[i];
+                }
+                data = temp;
+            }
+            data[++top] = num;
+        }
+        public override string ToString()
+        {
+            string s = "[";
+            for (int i = 0; i <= top; i++)
+            {
+                s += data[i];
+                if (i < top)
+                    s += ", ";
+            }
+            return s += "]";
+        }
+        public override bool Equals(object o)
+        {
+            if (o == null || !this.GetType().Equals(o.GetType()))
+                return false;
+            Stack1 s = (Stack1)o;//kasting
+            if (top != s.top) return false;
+            for (int i = 0; i <= top; i++)
+            {
+                if (data[i] != s.data[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
